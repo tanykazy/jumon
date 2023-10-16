052379b1b8108cf204bacbd2de51ad449d99a7f5
tanykazy <tanykazy@gmail.com>
2023-10-17 00:03:42 +0900

    Create code for your prototype

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module tanykazy/jumon
 
 go 1.19
+
+require (
+	github.com/atotto/clipboard v0.1.4
+	golang.org/x/term v0.13.0
+)
+
+require golang.org/x/sys v0.13.0 // indirect
--- jumon.go
+++ jumon.go
@@ -2,61 +2,114 @@ package main
 
 import (
 	"encoding/base64"
+	"flag"
 	"fmt"
+	"io"
 	"os"
+
+	"github.com/atotto/clipboard"
+	"golang.org/x/term"
 )
 
+var encodeFilename string
+var outputFilename string
+var useClipboard bool
+
+func init() {
+	// testing.Init()
+	flag.StringVar(&encodeFilename, "e", "", "エンコードするファイルを指定する")
+	flag.StringVar(&outputFilename, "o", "", "デコードのアウトプットファイル名を指定する")
+	flag.BoolVar(&useClipboard, "c", false, "エンコード結果をクリップボードに出力する")
+	flag.Parse()
+}
+
 func main() {
-	args := os.Args[1:]
-	if len(args) == 0 {
-		fmt.Println("Usage: base64 [-e ファイル] [-d 文字列]")
+
+	reader := os.Stdin
+
+	if term.IsTerminal(int(reader.Fd())) && encodeFilename == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+	if !term.IsTerminal(int(reader.Fd())) {
+		byte, err := io.ReadAll(reader)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if len(byte) > 0 {
+			data, err := decode(string(byte))
+
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+
+			if outputFilename == "" {
+				outputFilename = "output.bin"
+			}
+
+			err = os.WriteFile(outputFilename, data, 0644)
+
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+		}
 
 		return
 	}
 
-	switch args[0] {
-	case "-e":
-		encode(args[1])
-		break
-	case "-d":
-		decode(args[1])
-		break
-	default:
-		fmt.Println("Invalid argument")
+	if encodeFilename != "" {
+
+		if !fileExists(encodeFilename) {
+			fmt.Printf("%s が利用できません\n", encodeFilename)
+			return
+		}
+
+		data, err := encode(encodeFilename)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if useClipboard {
+			clipboard.WriteAll(data)
+		} else {
+			fmt.Println(data)
+		}
+
 		return
 	}
 }
 
-func encode(filename string) string {
+func encode(filename string) (string, error) {
 	data, err := os.ReadFile(filename)
 
 	if err != nil {
-		fmt.Println("Error:", err)
-		return ""
+		return "", err
 	}
 
 	encoded := base64.StdEncoding.EncodeToString(data)
 
-	fmt.Println(encoded)
-
-	return encoded
+	return encoded, nil
 }
 
-func decode(str string) []byte {
+func decode(str string) ([]byte, error) {
 	data, err := base64.StdEncoding.DecodeString(str)
 
 	if err != nil {
-		fmt.Println("Error:", err)
-		return nil
+		return nil, err
 	}
 
-	// fmt.Println(string(data))
+	return data, nil
+}
 
-	err = os.WriteFile("output.bin", data, 0644)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return nil
-	}
-
-	return data
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
 }
